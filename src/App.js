ef016ab394c77eb456e0ce1ba2f35b5bf8d32436
unknown <moalshaar225@gmail.com>
2025-05-16 15:32:25 +0300

    challenge three done! -use rendering List to render posts and buttons-

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import Header from "./Header";
 import Post from "./Post";
 import Sidemenu from "./Sidemenu";
 function App() {
+  const posts = [
+    {
+      id: 1,
+      title: "أكاديمية ترميز",
+      number: 20,
+      description: "أكاديمية مخصصة لتعلم البرمجة",
+    },
+    {
+      id: 2,
+      title: "hello world",
+      description: "this is the hello world article",
+    },
+    { id: 3, title: "Post 3", description: "this is the body of post 3" },
+  ];
+  const postsList = posts.map((post) => {
+    return (
+      <Post
+        key={post.id}
+        number={post.number}
+        title={post.title}
+        description={post.description}
+      />
+    );
+  });
   return (
     <div className="App">
       <Header />
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div style={{ display: "flex", width: "60%" }}>
-          <div style={{ width: "70%" }}>
-            <Post
-              number="20"
-              title="أكاديمية ترميز"
-              description="أكاديمية مخصصة لتعليم البرمجة"
-            />
-            <Post
-              title="hello world"
-              description="this is the hello world article"
-            />
-            <Post title="Post 3" description="this is the body of post 3" />
-          </div>
+          <div style={{ width: "70%" }}>{postsList}</div>
           <div style={{ width: "30%" }}>
             <Sidemenu />
           </div>
--- src/Sidemenu.js
+++ src/Sidemenu.js
@@ -9,29 +9,50 @@ export default function Sidemenu() {
     justifyContent: "center",
     flexWrap: "wrap",
   };
+  const buttons = [
+    {
+      id: 1,
+      child: (
+        <>
+          <p>جديدة</p>
+          <span>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+          </span>
+        </>
+      ),
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          <p>الأكثر قراءة</p>
+          <img src="../public/pexels-pixabay-50594.jpg" alt="flower" />
+        </>
+      ),
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          <p>مقالات مميزة</p>
+          <span>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+          </span>
+          <img src="../public/pexels-pixabay-50594.jpg" alt="flower" />
+        </>
+      ),
+    },
+  ];
+  const buttonsList = buttons.map((button) => {
+    return <Button key={button.id}>{button.child}</Button>;
+  });
   return (
     <div className={"Side"} style={sideStyle}>
-      <Button>
-        <p>جديدة</p>
-        <span>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-        </span>
-      </Button>
-      <Button>
-        <p>الأكثر قراءة</p>
-        <img src="../public/pexels-pixabay-50594.jpg" alt="flower" />
-      </Button>
-      <Button>
-        <p>مقالات مميزة</p>
-        <span>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-          <i className="fa-solid fa-star"></i>
-        </span>
-        <img src="../public/pexels-pixabay-50594.jpg" alt="flower" />
-      </Button>
+      {buttonsList}
     </div>
   );
 }
